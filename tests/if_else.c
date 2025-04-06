eef3510f53846c5b2ad42c74729ba935d0a01206
vaibhavi <vaibhavim081203@gmail.com>
2025-04-06 16:51:34 +0530

    tac for function done and functions working

--- tests/if_else.c
+++ tests/if_else.c
@@ -1,12 +1,10 @@
-
+int func(int x,int y){
+   int sum= x+y;
+   return x+y;
+}
+//void print(int b);
 int main() {
    int a=9;
    int b=10;
-   if(a&&b){
-    int x=a+b;
-    b=x*b;
-   }
-   else{
-    int y=10;
-   }
+   a=func(a,b);
 }
